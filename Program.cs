3b8e1062683a92470344ffbc1b3761b24937dffb
xxaewq <109620848+xxaewq@users.noreply.github.com>
2025-07-03 15:21:28 +0700

    Add files via upload
    commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿
+using MongoDB.Driver;
+using SignalR.Hubs;
+
+namespace SignalR
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddSignalR();
+
+            // Cấu hình Session
+            builder.Services.AddDistributedMemoryCache();
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromHours(6);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+            builder.Services.AddSingleton<IMongoClient>(sp =>
+            {
+                var connectionString = builder.Configuration.GetConnectionString("MongoDB");
+                var mongoClientSettings = MongoClientSettings.FromConnectionString(connectionString);
+                return new MongoClient(mongoClientSettings);
+            });
+
+            // Đăng ký IMongoDatabase
+            builder.Services.AddSingleton(sp =>
+            {
+                var client = sp.GetRequiredService<IMongoClient>();
+                return client.GetDatabase("chat");
+            });
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseSession(); // Kích hoạt session
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+            app.MapHub<HubChat>("/chatHub");
+
+            app.Run();
+        }
+    }
+}
